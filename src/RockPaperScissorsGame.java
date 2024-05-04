855e8ca73a19d34f53c23b3069592e7ff71474a7
Arjit katiyar <arjitkatiyar395@gmail.com>
2024-05-04 11:31:17 +0530

    added new features like can play continuously no need to run code again and again also user can only choose r, p, s if not choose these letter it will show invlid choice

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -3,29 +3,41 @@ import java.util.Scanner;
 
 public class RockPaperScissorsGame {
     public static void main(String[] args) {
-
-        System.out.println();                       // For New Line;
-        System.out.println("0 for Rock : 1 for Paper : 2 for Scissors");
-        System.out.println();                       // For New Line;
-
-        System.out.print("Enter Your Move : ");    //Enter zero || one || two;
         Scanner sc = new Scanner(System.in);
 
-        int userMove = sc.nextInt();              //taking input for user.
-        System.out.println("Your move is : " + userMove);
+        while (true) {
+            System.out.println();                       // For New Line;
+            System.out.println("choose : r for Rock : p for Paper : s for Scissors");
+            System.out.println();                       // For New Line;
+            String userMove;
 
-        Random rm = new Random();
-        int compMove = rm.nextInt(3);      //Taking computer move as Random.
-        System.out.println("Computer move is : " + compMove);
+            while (true){
+            System.out.print("Enter Your Move : ");     //Enter r || p || s;
+            userMove = sc.nextLine();                   //taking input for user.
+                if (userMove.equals("r") || userMove.equals("p") || userMove.equals("s")) {
+                    break;
+                }
+                System.out.println(userMove + " is not a valid move.");
+            }
+            System.out.println("Your move is : " + userMove);
 
-        if (userMove == compMove){
-            System.out.println("Match Draw ! ... Play Again");
-        } else if (userMove == 0 && compMove == 1 || userMove == 1 && compMove == 2 || userMove == 2 && compMove == 0) {
-            System.out.println("you Loose ! ... play Again");
-        }
-        else {
-            System.out.println("you Win ! ... play Again");
+            String []rps = {"r", "p", "s"};
+            String compMove = rps[new Random().nextInt(rps.length)];      //Taking computer move as Random.
+            System.out.println("Computer move is : " + compMove);
 
+            if (userMove.equals(compMove)) {
+                System.out.println("Match Draw ! ");
+            } else if (userMove.equals("r") && compMove.equals("p") || userMove.equals("p") && compMove.equals("s") || userMove.equals("s") && compMove.equals("r")) {
+                System.out.println("you Loose ! ");
+            } else {
+                System.out.println("you Win ! ");
+
+            }
+            System.out.println("Wanna play Again? (Y/N)");
+            String playAgain = sc.nextLine();
+            if (!playAgain.equals("y")){
+                break;
+            }
         }
     }
 }

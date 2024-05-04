9e9279555b76b481782adf40830b66d392109a6a
Arjit katiyar <arjitkatiyar395@gmail.com>
2024-05-04 11:50:44 +0530

    closed Scanner class

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -6,14 +6,14 @@ public class RockPaperScissorsGame {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println();                       // For New Line;
+            System.out.println(); // For New Line;
             System.out.println("choose : r for Rock : p for Paper : s for Scissors");
-            System.out.println();                       // For New Line;
+            System.out.println(); // For New Line;
             String userMove;
 
-            while (true){
-            System.out.print("Enter Your Move : ");     //Enter r || p || s;
-            userMove = sc.nextLine();                   //taking input for user.
+            while (true) {
+                System.out.print("Enter Your Move : "); // Enter r || p || s;
+                userMove = sc.nextLine(); // taking input for user.
                 if (userMove.equals("r") || userMove.equals("p") || userMove.equals("s")) {
                     break;
                 }
@@ -21,13 +21,14 @@ public class RockPaperScissorsGame {
             }
             System.out.println("Your move is : " + userMove);
 
-            String []rps = {"r", "p", "s"};
-            String compMove = rps[new Random().nextInt(rps.length)];      //Taking computer move as Random.
+            String[] rps = { "r", "p", "s" };
+            String compMove = rps[new Random().nextInt(rps.length)]; // Taking computer move as Random.
             System.out.println("Computer move is : " + compMove);
 
             if (userMove.equals(compMove)) {
                 System.out.println("Match Draw ! ");
-            } else if (userMove.equals("r") && compMove.equals("p") || userMove.equals("p") && compMove.equals("s") || userMove.equals("s") && compMove.equals("r")) {
+            } else if (userMove.equals("r") && compMove.equals("p") || userMove.equals("p") && compMove.equals("s")
+                    || userMove.equals("s") && compMove.equals("r")) {
                 System.out.println("you Loose ! ");
             } else {
                 System.out.println("you Win ! ");
@@ -35,9 +36,10 @@ public class RockPaperScissorsGame {
             }
             System.out.println("Wanna play Again? (Y/N)");
             String playAgain = sc.nextLine();
-            if (!playAgain.equals("y")){
+            if (!playAgain.equals("y")) {
                 break;
             }
         }
+        sc.close();
     }
 }
